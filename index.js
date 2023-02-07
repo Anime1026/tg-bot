ab06c808dd84b83af5c2cf844562dd2b4288c891
superstar323 <carlosarthur0323@gmail.com>
2023-02-08 04:20:12 +0900

    update

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const searchCollection_collectionId = (id) => {
+  const options = {
+    method: "GET",
+    headers: { accept: "*/*", "x-api-key": "demo-api-key" },
+  };
+
+  fetch(`https://api.reservoir.tools/collections/v5?id=${id}`, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+};
+
 bot.start((ctx) => {
   let message = `Please use the /eth or /sol command to receive a new nft`;
   ctx.reply(message);
@@ -12,7 +24,21 @@ bot.start((ctx) => {
 
 bot.command("eth", async (ctx) => {
   try {
-    ctx.reply("Please Input the CollectionName or Contract Address");
+    ctx.reply(
+      "Please use the /ethId or /ethName command to search NFT Collections"
+    );
+  } catch (error) {
+    console.log("error", error);
+    ctx.reply("Sorry, please again");
+  }
+});
+
+bot.command("ethId", async (ctx) => {
+  try {
+    ctx.reply("Please Input the CollectionName");
+    bot.on(text, async (ctx) => {
+      console.log(text, "text");
+    });
   } catch (error) {
     console.log("error", error);
     ctx.reply("Sorry, please again");
