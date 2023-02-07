b04bf1be29c9aff76c2b3b000f902fb7d7d9adfe
superstar323 <carlosarthur0323@gmail.com>
2023-02-08 04:50:04 +0900

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const { Telegraf } = require("telegraf");
-const { v4: uuidV4 } = require("uuid");
-require("dotenv").config();
+import fetch from "node-fetch";
+import { Telegraf } from "telegraf";
+import { v4 as uuidV4 } from "uuid";
+
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 // let factGenerator = require("./factGenerator");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
